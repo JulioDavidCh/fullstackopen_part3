fe2bde84916215bbaf3d80a9c8edeee81bcad310
JulioDavidCd <juliodavidchopite@gmail.com>
2019-12-01 20:34:30 -0300

    frontend configuration

---
+++ build/precache-manifest.a9778743acb128dc5f55197f33b7e159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91c7aa6258f511d342a39cd551a56ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "037fbabd280fa9204d29",
+    "url": "/static/css/main.8bcffd3d.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "037fbabd280fa9204d29",
+    "url": "/static/js/main.b4c60beb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+app.use(express.static('build'))
+
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
